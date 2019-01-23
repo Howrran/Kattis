81121a1b0b2e03d5c021213d9c74d2a9746ec2be
Howrran <37328064+Howrran@users.noreply.github.com>
2019-01-23 03:22:36 +0200

    fix

--- Algotester/Числа_для_Малят.py
+++ Algotester/Числа_для_Малят.py
@@ -1,29 +1,15 @@
-import time
-def foo(n):
-    # n = input()
-    time.sleep(1)
-    s = ''.join(sorted(n))
+n = input()
+s = ''.join(sorted(n))
 
-    if len(s) > 1 and s[0] == '0':
-        ss = list(s)
-        counter = 0
-        while (ss[counter] == '0'):
-            counter += 1
-        ss[0], ss[counter] = ss[counter], ss[0]
-        s = ''.join(ss)
+if len(s) > 1 and s[0] == '0':
+    ss = list(s)
+    counter = 0
+    while (ss[counter] == '0'):
+        counter += 1
+    ss[0], ss[counter] = ss[counter], ss[0]
+    s = ''.join(ss)
 
-    min = s
-    max = ''.join(sorted(n, reverse=True))
+min = s
+max = ''.join(sorted(n, reverse=True))
 
-    print('{} {}'.format(min, max))
-    if max < min:
-        print('PZDCNAXYIBLED')
-        time.sleep(5)
-
-
-
-
-count = 1000
-while count < 10000000:
-    foo(str(count))
-    count += 1
+print('{} {}'.format(min, max))
